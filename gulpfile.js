0626cbfe8179ed5e7fc8b2a3acefa47668343269
Stanislav Gulya <admin@MacBook-Pro-Stanislav.local>
2020-05-06 20:13:32 +0600

    feat: create gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-
-gulp.task ('hello', function(done) {
- console.log('Привет, Мир!');
-});
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
- browserSync.init({
-     server: {
-         baseDir: "./"
+ function bs() {
+     serveSass
+     browserSync.init({
+         server: {
+             baseDir: "./"
      }
- });
- gulp.watch("./*.html").on('change', browserSync.reload);
- gulp.watch("./*.css").on('change', browserSync.reload);
-});
+   });
+ watch("./*.html").on('change', browserSync.reload);
+ watch("./sass/**/*.sass", serveSass);
+ watch("./js/*.js").on('change', browserSync.reload);
+};
 
-//Изменения в CSS
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs; 
